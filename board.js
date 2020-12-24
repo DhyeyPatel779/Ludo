bfdd58e3a1f3cd722b17d1628d834a625cd03afe
DhyeyPatel779 <71546105+DhyeyPatel779@users.noreply.github.com>
2020-12-24 19:59:48 +0530

    Add files via upload

---
+++ board.js
@@ -0,0 +1,7 @@
+class Board {
+    constructor(x,y) {
+    }
+    display(){
+
+    }
+  };
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var form
+var board
+
+function preload(){
+
+}
+
+function setup(){
+createCanvas(displayWidth-20,displayHeight-30);
+
+
+form = new Form();
+}
+
+
+function draw(){
+ form.display();
+}
